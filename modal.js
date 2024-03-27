34303403a9a493d52f0eff9fd5845d95f9835a28
GregoryLacroix <gregorylacroix78@gmail.com>
2024-03-27 18:34:53 +0100

    update javascript

--- modal.js
+++ modal.js
@@ -10,47 +10,51 @@ function editNav() {
 // DOM Elements
 const modalBody = document.querySelector('.modal-body');
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalBtnClose = document.querySelectorAll(".close");
-const form = document.getElementById("reserve");
-const firstName = document.getElementById("first");
-const lastName = document.getElementById("last");
-const email = document.getElementById("email");
-const birthdate = document.getElementById("birthdate");
-const quantity = document.getElementById("quantity");
-const agreeTerms = document.getElementById("checkbox1");
-const agreeTermsIcone = document.getElementById("checkbox-icon-agree-terms");
+const modalButton = document.querySelector(".modal-btn");
+const formData = document.querySelector(".formData");
+const modalArrowClose = document.querySelector(".close");
+const modalForm = document.querySelector("#reserve");
+const inputFirstName = document.querySelector("#first");
+const inputLastName = document.querySelector("#last");
+const inputEmail = document.querySelector("#email");
+const inputBirthdate = document.querySelector("#birthdate");
+const inputQuantity = document.querySelector("#quantity");
+const checkboxAgreeTerms = document.querySelector("#checkbox1");
+const checkboxAgreeTermsIcone = document.querySelector("#checkbox-icon-agree-terms");
 const checkboxLocation = document.querySelectorAll('.checkbox__location');
 const bodyFinalInscription = document.querySelector('.body__final__inscription');
-const btnClose = document.querySelectorAll('.btn-close');
+const modalButtonClose = document.querySelector('.btn-close');
 
-// message erreurs - css errors
-const messageErrorFirstName = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-const messageErrorLastName = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-const messageErrorEmail = "Veuillez saisir une adresse mail valide.";
-const messageErrorBirthdate = "Veuillez saisir une date de naissance.";
-const messageErrorQuantity = "Veuillez saisir une quantité valide.";
-const messageErrorMaxQuantity = "Veuillez saisir une quantité comprise entre 0 et 99.";
-const messageErrorAgreeTerms = "Vous devez accepter les conditions d'utilisation.";
-const messageErrorLocation = "Vous devez choisir une option.";
+// message erreurs
+const MESSAGE_ERROR = {
+  firstName: "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
+  lastName: "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
+  email: "Veuillez saisir une adresse mail valide.",
+  birthdate: "Veuillez saisir une date de naissance.",
+  quantity: "Veuillez saisir une quantité valide.",
+  agreeTerms: "Vous devez accepter les conditions d'utilisation.",
+  location: "Vous devez choisir une option."
+}
+
+// css errors
 const borderError = "2px solid #FF4E60";
 
 // Regex Email
 const regexEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
 // Injection des messages d'erreurs
-const errorFirstName = document.querySelector('.error__first__name');
-const errorLastName = document.querySelector('.error__last__name');
-const errorEmail = document.querySelector('.error__email');
-const errorBirthdate = document.querySelector('.error__birthdate');
-const errorQuantity = document.querySelector('.error__quantity');
-const errorAgreeTerms = document.querySelector('.error__agree__terms');
-const errorLocation = document.querySelector('.error__location');
+const labelFirstName = document.querySelector('.error__first__name');
+const labelLastName = document.querySelector('.error__last__name');
+const labelEmail = document.querySelector('.error__email');
+const labelBirthdate = document.querySelector('.error__birthdate');
+const labelQuantity = document.querySelector('.error__quantity');
+const labelAgreeTerms = document.querySelector('.error__agree__terms');
+const labelLocation = document.querySelector('.error__location');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-modalBtnClose.forEach((btn) => btn.addEventListener("click", closeModal));
+// modalButton.forEach((btn) => btn.addEventListener("click", launchModal));
+modalButton.addEventListener("click", launchModal);
+modalArrowClose.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
@@ -67,89 +71,91 @@ function closeModal() {
   }, 1000);
 }
 
+function textFieldEnteredHandler(input, label, message, border, error) {
+  input.style.border = "none";
+  label.innerHTML = "";
+  if(input.value.length == 0 || input.value.length <= 2){
+    label.innerHTML = message;
+    input.style.border = border;
+    error = error;
+  }
+}
+
+function customCssInit(label, input = null) {
+  if(input != null)
+    input.style.border = "none";
+
+  label.innerHTML = "";
+}
+
 // Contrôle des données saisie dans le formulaire d'inscription
-form.addEventListener("submit", submitFormModal);
+modalForm.addEventListener("submit", submitFormModal);
 function submitFormModal(event) {
   event.preventDefault();
 
+  let error = false;
+
   // Contrôle de la validité du prénom
-  firstName.style.border = "none";
-  errorFirstName.innerHTML = "";
-  if(firstName.value.length == 0 || firstName.value.length <= 2){
-    errorFirstName.innerHTML = messageErrorFirstName;
-    firstName.style.border = borderError;
-    var error = true;
-  }
+  textFieldEnteredHandler(inputFirstName, labelFirstName, MESSAGE_ERROR.firstName, borderError, true);
 
   // Contrôle de la validité du nom
-  lastName.style.border = "none";
-  errorLastName.innerHTML = "";
-  if(lastName.value.length == 0 || lastName.value.length <= 2){
-    errorLastName.innerHTML = messageErrorLastName;
-    lastName.style.border = borderError;
-    var error = true;
-  }
+  textFieldEnteredHandler(inputLastName, labelLastName, MESSAGE_ERROR.lastName, borderError, true);
 
   // Contrôle de la validité de l'adresse Email
-  email.style.border = "none";
-  errorEmail.innerHTML = "";
-  if (!regexEmail.test(email.value)) {
-    errorEmail.innerHTML = messageErrorEmail;
-    email.style.border = borderError;
-    var error = true;
+  customCssInit(labelEmail, inputEmail);
+  if (!regexEmail.test(inputEmail.value)) {
+    labelEmail.innerHTML = MESSAGE_ERROR.email;
+    inputEmail.style.border = borderError;
+    error = true;
   }
 
   // Contrôle de la validité de la date de naissance
-  birthdate.style.border = "none";
-  errorBirthdate.innerHTML = "";
-  if(birthdate.value.length == 0){
-    errorBirthdate.innerHTML = messageErrorBirthdate;
-    birthdate.style.border = borderError;
-    var error = true;
+  customCssInit(labelBirthdate, inputBirthdate);
+  if(inputBirthdate.value.length == 0){
+    labelBirthdate.innerHTML = MESSAGE_ERROR.birthdate;
+    inputBirthdate.style.border = borderError;
+    error = true;
   }
 
   // Contrôle de la validité du nombre de tournoi
-  quantity.style.border = "none";
-  errorQuantity.innerHTML = "";
-  if(quantity.value.length == 0 || (typeof(quantity.value) != 'number' && isNaN(quantity.value))){
-    errorQuantity.innerHTML = messageErrorQuantity;
-    quantity.style.border = borderError;
-    var error = true;
-  }else if(quantity.value > 100){
-    errorQuantity.innerHTML = messageErrorMaxQuantity;
-    quantity.style.border = borderError;
-    var error = true;
+  customCssInit(labelQuantity, inputQuantity);
+  if(inputQuantity.value.length == 0 || (typeof(inputQuantity.value) != 'number' && isNaN(inputQuantity.value))){
+    labelQuantity.innerHTML = MESSAGE_ERROR.quantity;
+    inputQuantity.style.border = borderError;
+    error = true;
   }
 
   // Contrôle de la validation des conditions d'utilisation
-  agreeTermsIcone.style.border = "none";
-  errorAgreeTerms.innerHTML = "";
-  if(!agreeTerms.checked){
-    errorAgreeTerms.innerHTML = messageErrorAgreeTerms;
-    agreeTermsIcone.style.border = borderError;
-    var error = true;
+  customCssInit(labelAgreeTerms, checkboxAgreeTermsIcone);
+  if(!checkboxAgreeTerms.checked){
+    labelAgreeTerms.innerHTML = MESSAGE_ERROR.agreeTerms;
+    checkboxAgreeTermsIcone.style.border = borderError;
+    error = true;
   }
   
   // Contrôle de saisie de checkbox pour le lieu du tournoi
-  let total = 0;
+  var array = []
   checkboxLocation.forEach(function (item) {
+    let checked = false;
     if(item.checked){
-      total += total + 1;
+      checked = true;
     }
+    array.push(checked);
   });
+  const result = array.some((elem) => elem == true);
 
-  errorLocation.innerHTML = "";
-  if(total == 0){
-    errorLocation.innerHTML = messageErrorLocation;
-    var error = true;
+  customCssInit(labelLocation);
+  if(result == false){
+    labelLocation.innerHTML = MESSAGE_ERROR.location;
+    error = true;
   }
 
   // Validation du formulaire si tout les champs sont correctement rempli
-  if(!error){
+  if(error == false){
     bodyFinalInscription.classList.add('body__modal__open');
     modalBody.classList.add('body__modal__close');
-    btnClose.forEach((btn) => btn.addEventListener("click", closeModal));
-    form.reset();
+    modalButtonClose.addEventListener("click", closeModal);
+    modalForm.reset();
   }
 }
 
